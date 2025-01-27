5432460ea57d75a50c2eeab359796d2f194dd623
Anthony Silva <ajsilva228@outlook.com>
2025-01-26 22:30:10 -0800

    Added problem 1280

---
+++ SQL/1280-Students-and-Examinations/1280.sql
@@ -0,0 +1,12 @@
+SELECT 
+    Students.student_id, 
+    Students.student_name, 
+    Subjects.subject_name, 
+    COUNT(Examinations.student_id) attended_exams
+FROM Students
+    CROSS JOIN Subjects
+    LEFT JOIN Examinations ON
+        Students.student_id = Examinations.Student_id AND
+        Subjects.subject_name = Examinations.subject_name
+GROUP BY Students.student_id, Students.student_name, Subjects.subject_name
+ORDER BY Students.student_id, Students.student_name, Subjects.subject_name
